9384c35215c39fe1b5320deac9b1e4e0bd8328e9
evondev <trananhtuan400@gmail.com>
2021-10-04 11:54:46 +0700

    udate jalan source code

--- jalan/app.js
+++ jalan/app.js
@@ -6,3 +6,23 @@ toggleClose.addEventListener("click", handleToggleMenu);
 function handleToggleMenu(e) {
   menu.classList.toggle("is-show");
 }
+$(".offer-list").slick({
+  infinite: true,
+  slidesToShow: 4,
+  arrows: false,
+  centerMode: true,
+  slidesToScroll: -1,
+  centerPadding: "0",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        centerMode: true,
+        centerPadding: "30px",
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+  ],
+});
