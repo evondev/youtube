6cd718ae25e66f55abb8a069784c63a1d8cbe785
evondev <trananhtuan400@gmail.com>
2021-12-14 20:22:02 +0700

    update mp3 player

--- mp3/app.js
+++ mp3/app.js
@@ -2,9 +2,14 @@ const song = document.getElementById("song");
 const playBtn = document.querySelector(".player-inner");
 const nextBtn = document.querySelector(".play-forward");
 const prevBtn = document.querySelector(".play-back");
+const durationTime = document.querySelector(".duration");
+const remainingTime = document.querySelector(".remaining");
+const rangeBar = document.querySelector(".range");
 let isPlaying = true;
 let indexSong = 0;
 const musics = ["holo.mp3", "summer.mp3", "spark.mp3", "home.mp3"];
+displayTimer();
+let timer;
 song.setAttribute("src", `./music/${musics[indexSong]}`);
 nextBtn.addEventListener("click", function () {
   changeSong(1);
@@ -12,6 +17,10 @@ nextBtn.addEventListener("click", function () {
 prevBtn.addEventListener("click", function () {
   changeSong(-1);
 });
+song.addEventListener("ended", handleEndedSong);
+function handleEndedSong() {
+  changeSong(1);
+}
 function changeSong(dir) {
   if (dir === 1) {
     // next song
@@ -37,9 +46,33 @@ function playPause() {
     song.play();
     playBtn.innerHTML = `<ion-icon name="pause-circle"></ion-icon>`;
     isPlaying = false;
+    timer = setInterval(displayTimer, 500);
   } else {
     song.pause();
     playBtn.innerHTML = `<ion-icon name="play"></ion-icon>`;
     isPlaying = true;
+    clearInterval(timer);
   }
+}
+function displayTimer() {
+  const { duration, currentTime } = song;
+  rangeBar.max = duration;
+  rangeBar.value = currentTime;
+  remainingTime.textContent = formatTimer(currentTime);
+  if (!duration) {
+    durationTime.textContent = "00:00";
+  } else {
+    durationTime.textContent = formatTimer(duration);
+  }
+}
+function formatTimer(number) {
+  const minutes = Math.floor(number / 60);
+  const seconds = Math.floor(number - minutes * 60);
+  return `${minutes < 10 ? "0" + minutes : minutes}:${
+    seconds < 10 ? "0" + seconds : seconds
+  }`;
+}
+rangeBar.addEventListener("change", handleChangeBar);
+function handleChangeBar() {
+  song.currentTime = rangeBar.value;
 }
