9b25ddae0814c822b9e27a6cb46827c4feda895d
evondev <trananhtuan400@gmail.com>
2021-12-20 23:21:00 +0700

    update mp3 player part4

--- mp3/app.js
+++ mp3/app.js
@@ -5,12 +5,49 @@ const prevBtn = document.querySelector(".play-back");
 const durationTime = document.querySelector(".duration");
 const remainingTime = document.querySelector(".remaining");
 const rangeBar = document.querySelector(".range");
+const musicName = document.querySelector(".music-name");
+const musicThumbnail = document.querySelector(".music-thumb");
+const musicImage = document.querySelector(".music-thumb img");
+const playRepeat = document.querySelector(".play-repeat");
+
 let isPlaying = true;
 let indexSong = 0;
-const musics = ["holo.mp3", "summer.mp3", "spark.mp3", "home.mp3"];
-displayTimer();
+let isRepeat = false;
+// const musics = ["holo.mp3", "summer.mp3", "spark.mp3", "home.mp3"];
+const musics = [
+  {
+    id: 1,
+    title: "Holo",
+    file: "holo.mp3",
+    image:
+      "https://images.unsplash.com/photo-1614624532983-4ce03382d63d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80",
+  },
+  {
+    id: 2,
+    title: "Summer",
+    file: "summer.mp3",
+    image:
+      "https://images.unsplash.com/photo-1616763355548-1b606f439f86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+  },
+];
+/**
+ * Music
+ * id: 1
+ * title: Holo
+ * file: holo.mp3
+ * image: unsplash
+ */
 let timer;
-song.setAttribute("src", `./music/${musics[indexSong]}`);
+let repeatCount = 0;
+playRepeat.addEventListener("click", function () {
+  if (isRepeat) {
+    isRepeat = false;
+    playRepeat.removeAttribute("style");
+  } else {
+    isRepeat = true;
+    playRepeat.style.color = "#ffb86c";
+  }
+});
 nextBtn.addEventListener("click", function () {
   changeSong(1);
 });
@@ -19,7 +56,14 @@ prevBtn.addEventListener("click", function () {
 });
 song.addEventListener("ended", handleEndedSong);
 function handleEndedSong() {
-  changeSong(1);
+  repeatCount++;
+  if (isRepeat && repeatCount === 1) {
+    // handle repeat song
+    isPlaying = true;
+    playPause();
+  } else {
+    changeSong(1);
+  }
 }
 function changeSong(dir) {
   if (dir === 1) {
@@ -37,17 +81,20 @@ function changeSong(dir) {
     }
     isPlaying = true;
   }
-  song.setAttribute("src", `./music/${musics[indexSong]}`);
+  init(indexSong);
+  // song.setAttribute("src", `./music/${musics[indexSong].file}`);
   playPause();
 }
 playBtn.addEventListener("click", playPause);
 function playPause() {
   if (isPlaying) {
+    musicThumbnail.classList.add("is-playing");
     song.play();
     playBtn.innerHTML = `<ion-icon name="pause-circle"></ion-icon>`;
     isPlaying = false;
     timer = setInterval(displayTimer, 500);
   } else {
+    musicThumbnail.classList.remove("is-playing");
     song.pause();
     playBtn.innerHTML = `<ion-icon name="play"></ion-icon>`;
     isPlaying = true;
@@ -76,3 +123,10 @@ rangeBar.addEventListener("change", handleChangeBar);
 function handleChangeBar() {
   song.currentTime = rangeBar.value;
 }
+function init(indexSong) {
+  song.setAttribute("src", `./music/${musics[indexSong].file}`);
+  musicImage.setAttribute("src", musics[indexSong].image);
+  musicName.textContent = musics[indexSong].title;
+}
+displayTimer();
+init(indexSong);
